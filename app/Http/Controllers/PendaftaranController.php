84bc85d43580e94253f7f9bff8cf2301e463d755
Mochamad Idris <idrez.mochamad@gmail.com>
2025-05-08 07:33:23 +0000

    add 10120kb

--- app/Http/Controllers/PendaftaranController.php
+++ app/Http/Controllers/PendaftaranController.php
@@ -106,7 +106,7 @@ class PendaftaranController extends Controller
 				'jenis_pkm_id'	=> 'required|exists:jenis_pkm,id',
 				'tema_usulan_pkm_id'	=> 'required|exists:tema_usulan_pkm,id',
 				'berkas' => 'required|array', // Pastikan 'berkas' adalah array
-				'berkas.*' => 'file|mimes:pdf|max:5120', // Validasi setiap file dalam array
+				'berkas.*' => 'file|mimes:pdf|max:10120', // Validasi setiap file dalam array
 				'pegawai_id'	=> 'required',
 				'list_nim' => 'required|array|min:2|max:4',
 				'list_nim.*.nim' => 'required|distinct',
