3bd15de7952058ccb86b94f78c4f696906e98e51
Mochamad Idris <idrez.mochamad@gmail.com>
2025-05-08 15:08:39 +0700

    fix pilih ketua

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -209,6 +209,15 @@ class AdminController extends Controller
                 //             });
                 //     }); 
                 // })
+                ->whereDoesntHave('anggota_pkm', function (Builder $query) use ($request, $tahun, $jenis_pkm) {
+                    $query->whereHas('usulan_pkm', function (Builder $query) use ($request, $tahun, $jenis_pkm) {
+                        $query->where('usulan_pkm.tahun', $tahun)
+                            ->whereHas('jenis_pkm', function (Builder $query) use ($request, $jenis_pkm) {
+                                $query->where('jenis_pkm.kamar', $jenis_pkm->kamar)
+                                    ->where('jenis_pkm.kategori_kegiatan_id', $jenis_pkm->kategori_kegiatan_id);
+                            });
+                    }); 
+                })
                 ->get(['nim AS id', DB::raw('CONCAT(nama," ","[",nim,"]"," ","[",nama_forlap,"]"," ","[",nama_fak_ijazah,"]") as text')]);
         }
 
