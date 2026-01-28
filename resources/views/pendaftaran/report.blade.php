4e273f63cc33ea16321590bab0a7b686a01e0f5f
Mochamad Idris <idrez.mochamad@gmail.com>
2026-01-28 17:50:53 +0700

    fix(report): menambahkan hp dan email

--- resources/views/pendaftaran/report.blade.php
+++ resources/views/pendaftaran/report.blade.php
@@ -5,25 +5,25 @@ header("Content-Disposition: attachment; filename=\"usulan_pkm.xls\"");
 <table border="1">
     <thead>
         <tr>
-            <th colspan="13">Laporan Pengajuan PKM</th>
+            <th colspan="18">Laporan Pengajuan PKM</th>
         </tr>
         <tr>
-            <th colspan="13">Universitas Diponegoro</th>
+            <th colspan="18">Universitas Diponegoro</th>
         </tr>
         <tr>
-            <th colspan="13">Tahun {{ $tahun }}</th>
+            <th colspan="18">Tahun {{ $tahun }}</th>
         </tr>
         <tr>
-            <th colspan="13">&nbsp;</th>
+            <th colspan="18">&nbsp;</th>
         </tr>
         <tr>
             <th rowspan="2">No</th>
             <th rowspan="2">Judul</th>
-            <th colspan="5">Mahasiswa</th>
+            <th colspan="7">Mahasiswa</th>
             <th rowspan="2">Kegiatan</th>
             <th rowspan="2">SubKegiatan</th>
             <th rowspan="2">Tema</th>
-            <th colspan="2">Dosen Pendamping</th>
+            <th colspan="4">Dosen Pendamping</th>
             <th rowspan="2">File Proposal</th>
             <th rowspan="2">SubmittedAt</th>
         </tr>
@@ -33,8 +33,12 @@ header("Content-Disposition: attachment; filename=\"usulan_pkm.xls\"");
             <th>Jabatan</th>
             <th>Fakultas</th>
             <th>Prodi</th>
+            <th>Email</th>
+            <th>No Telp</th>
             <th>Nama</th>
             <th>NUPTK</th>
+            <th>Email</th>
+            <th>Hp</th>
         </tr>
     </thead>
     <tbody>
@@ -50,11 +54,15 @@ header("Content-Disposition: attachment; filename=\"usulan_pkm.xls\"");
             <td><span>{{ "Ketua" }}</span></td>
             <td><span>{{ $mhs->nama_fak_ijazah }}</span></td>
             <td><span>{{ $mhs->nama_forlap }}</span></td>
+            <td><span>{{ $usulan_pkm->mhs_email }}</span></td>
+            <td><span>{{ "'" . $usulan_pkm->mhs_no_telp }}</span></td>
             <td rowspan="{{ $usulan_pkm->anggota_pkm()->count() }}"><span>{{ $usulan_pkm->jenis_pkm->kategori_kegiatan->nama_kategori_kegiatan }}</span></td>
             <td rowspan="{{ $usulan_pkm->anggota_pkm()->count() }}"><span>{{ $usulan_pkm->jenis_pkm->nama_pkm }}</span></td>
             <td rowspan="{{ $usulan_pkm->anggota_pkm()->count() }}"><span>{{ !empty($usulan_pkm->tema_usulan_pkm_id) ? $usulan_pkm->tema_usulan_pkm->nama_tema : "-" }}</span></td>
             <td rowspan="{{ $usulan_pkm->anggota_pkm()->count() }}"><span style="white-space: nowrap;">{{ $usulan_pkm->pegawai->glr_dpn . ' ' . $usulan_pkm->pegawai->nama . ' ' . $usulan_pkm->pegawai->glr_blkg }}</span></td>
             <td rowspan="{{ $usulan_pkm->anggota_pkm()->count() }}"><span>{{ "'". $usulan_pkm->pegawai->nuptk }}</span></td>
+            <td rowspan="{{ $usulan_pkm->anggota_pkm()->count() }}"><span>{{ $usulan_pkm->pegawai_email_sso }}</span></td>
+            <td rowspan="{{ $usulan_pkm->anggota_pkm()->count() }}"><span>{{ "'" . $usulan_pkm->pegawai_hp }}</span></td>
             <td rowspan="{{ $usulan_pkm->anggota_pkm()->count() }}">
                 @foreach ($usulan_pkm->usulan_pkm_dokumen()->take(1)->get() as $i => $usulan_pkm_dokumen)
                 <b style="display:block;"><a href="{{ asset('storage/' . $usulan_pkm_dokumen->document_path ) }}" target="_blank">{{ 'dokumen('. ($i + 1) . ')' }}</a></span>, 
@@ -69,6 +77,8 @@ header("Content-Disposition: attachment; filename=\"usulan_pkm.xls\"");
             <td><span>{{ "Anggota" }}</span></td>
             <td><span>{{ $anggota_pkm->mhs->nama_fak_ijazah }}</span></td>
             <td><span>{{ $anggota_pkm->mhs->nama_forlap }}</span></td>
+            <td><span style="background-color: #aaaaaa;">&nbsp;</span></td>
+            <td><span style="background-color: #aaaaaa;">&nbsp;</span></td>
         </tr>
         @endforeach
         @endforeach
